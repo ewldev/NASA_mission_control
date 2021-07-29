b8f1520ce8c35107832bc70b780865a5a9ebf6cf
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-07-28 23:59:36 -0400

    modified app.js added launches.model.js launches router and controller.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router')
+const launchesRouter = require('./routes/launches/launches.router');
+const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use(launchesRouter);
 app.use(planetsRouter);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
